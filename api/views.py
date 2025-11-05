1e3a639146ab387202a0615470a19bc61999cb2b
user <user@Kali.Kali>
2025-11-05 18:35:26 +0600

    pip

--- api/views.py
+++ api/views.py
@@ -5,6 +5,6 @@ from .serializers import OnboardingSerializer
 from rest_framework.permissions import AllowAny
 
 class OnboardingListView(generics.ListAPIView):
-    permission_classes = (AllowAny)
+    permission_classes = [AllowAny]
     queryset = Onboarding.objects.all()
     serializer_class = OnboardingSerializer
